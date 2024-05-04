38384c79395a18b8f542f102ab512d54fddb84a4
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-04 11:00:33 +0500

    Footer is completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavbarWeb from './components/NavbarWeb';
 import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
    <>
    <NavbarWeb />
    <Carousel />
+   <Footer />
    </>
   );
 }
