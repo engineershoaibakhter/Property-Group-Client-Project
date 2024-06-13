fe11a4b043cd581956bc6ee996d846f3a15ab5f7
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-06-13 18:27:52 +0500

    First Website is completed

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -9,23 +9,7 @@ const Contact = () => {
       <div className={styles.content}>
         <div className={`${styles.leftContent} ${styles.leftSide}`}>
           <img src='/images/contact-image.jpg' alt='contact-image' width="70%" height="70%"/>
-          {/* Add your left content here */}
-          {/* <div className={`${styles.details} ${styles.address}`}>
-            <i className="fas fa-map-marker-alt"></i>
-            <div className={styles.topic}>Address</div>
-            <div className={styles.textOne}>Level 8/468 St Kilda Road</div>
-            <div className={styles.textTwo}>Melbourne VIC 3004</div>
-          </div>
-          <div className={`${styles.details} ${styles.phone}`}>
-            <i className="fas fa-phone-alt"></i>
-            <div className={styles.topic}>Phone</div>
-            <div className={styles.textOne}>03 9222 4000</div>
-          </div>
-          <div className={`${styles.details} ${styles.email}`}>
-            <i className="fas fa-envelope"></i>
-            <div className={styles.topic}>Email</div>
-            <div className={styles.textOne}>michael.ellis@bcacorp.com.au</div>
-          </div> */}
+         
         </div>
         <div className={`${styles.rightContent} ${styles.rightSide}`}>
           <FormExample />
