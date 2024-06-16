203a11ade414019319664516267db77366156b59
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-06-16 08:54:07 +0500

    partners section are completed

--- src/app/components/Partners.js
+++ src/app/components/Partners.js
@@ -1,18 +1,33 @@
 "use client";
-import React from 'react';
+// components/Partners.js
 import styles from '../styles/Partners.module.css';
 
 const Partners = () => {
+  const images = [
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png',
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png',
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png',
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png',
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png',
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png',
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png',
+  ];
+
   return (
-    <div className={styles.partners}>
-        <h1 className={styles.heading}>Our Partners</h1>
-    <div className={styles.container}>
-      <div className={styles.sliderWrapper}>
-        <ul className={styles.imageList}>
-          <li className={styles.imageItem}><img src="/images/partner1.png" alt="img-1" /></li>
-          <li className={styles.imageItem}><img src="/images/landmark.png" alt="img-2" /></li>
-          <li className={styles.imageItem}><img src="/images/partner1.png" alt="img-3" /></li>
-        </ul>
+    <div className={styles.Partners}>
+      <h2 className={styles.h2}>Our Partners</h2>
+    <div className={styles.slider}>
+      <div className={styles.slideTrack}>
+        {images.map((src, index) => (
+          <div className={styles.slide} key={index}>
+            <img src={src} height="100" width="250" alt={`Partner ${index + 1}`} />
+          </div>
+        ))}
+        {images.map((src, index) => (
+          <div className={styles.slide} key={index + images.length}>
+            <img src={src} height="100" width="250" alt={`Partner ${index + 1}`} />
+          </div>
+        ))}
       </div>
     </div>
     </div>
